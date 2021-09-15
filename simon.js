d53ae5f93a70ad049ce94b291bf6f2337197046c
Paul J. Athmer <Paul,Athmer@gmail.com>
2021-09-15 14:02:22 -0700

    some added work

--- simon.js
+++ simon.js
@@ -1,30 +1,35 @@
 
 function startGame(){
 	let playerInput = true;
+	let simonArr = [];
 		
 	while(playerInput){
+		let level = 1;
 		let random = (Math.floor(Math.random()*4));
 		console.log(random);
 		//play corresponding color/sound
-		let simonArr = [];
 		simonArr.push(random);
 		
-		for(let i = 0; i < simonArr; i++){
+		for(let i = 0; i < simonArr.length; i++){
 			//play audio
 			if(simonArr[i]==0)
 			{
+				// 0 = green
 				//play audio 0;
 			}			
 			if(simonArr[i]==1)
 			{
+				// 1 = red
 				//play audio 1;
 			}
 			if(simonArr[i]==2)
 			{
+				// 2 = yellow
 				//play audio 2;
 			}
 			if(simonArr[i]==3)
 			{
+				// 3 = blue 
 				//play audio 3;
 			}
 			
